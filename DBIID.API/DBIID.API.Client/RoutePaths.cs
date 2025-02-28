cce9a190c603c4a7b48b58360a944c640bf689cf
Jonas D. Pedersen <jdp@brandogsikring.dk>
2025-02-28 12:01:21 +0100

    Routes tilføjet til Blazor frontend

---
+++ DBIID.API/DBIID.API.Client/RoutePaths.cs
@@ -0,0 +1,17 @@
+﻿public static class RoutePaths
+{
+    public const string Home = "/";
+    public const string Counter = "/counter";
+    public const string Weather = "/weather";
+
+    public static class User
+    {
+        public const string Base = "/user";
+        public const string List = $"{Base}/list";
+        public const string Create = $"{Base}/create";
+        public static string Details(int userId) => $"{Base}/{userId}";
+        public static string Permissions(int userId) => $"{Details(userId)}/permissions";
+    }
+
+
+}
